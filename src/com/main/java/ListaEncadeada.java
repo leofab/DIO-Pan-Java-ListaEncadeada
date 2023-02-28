0fae3297df9328892982e9ad4497b5f1d9144262
leofab <leofabr3@gmail.com>
2023-02-28 12:03:14 -0300

    method remove implement

--- src/com/main/java/ListaEncadeada.java
+++ src/com/main/java/ListaEncadeada.java
@@ -38,6 +38,18 @@ public class ListaEncadeada<T> {
 		return noRetorno;
 	}
 	
+	public T remove(int index) {
+		No<T> noPivo = this.getNo(index);
+		if(index == 0) {
+			referenciaEntrada = noPivo.getProximoNo();
+			return noPivo.getConteudo();
+			
+			}
+		No<T> noAnterior = getNo(index - 1);
+		noAnterior.setProximoNo(noPivo.getProximoNo());
+		return noPivo.getConteudo();
+	}
+	
 	
 	public int size(){
 		int tamanhoLista = 0;
